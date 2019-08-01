f2228eb9dca74efae3ad2d28d92bfdb6e9ca3af5
Yebin Kim <girincute@gmail.com>
2019-08-01 23:03:57 +0900

    Use templates

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'author1',
+        'title': 'Post 1',
+        'content': 'First content',
+        'date_posted': 'August 1, 2019'
+    }, 
+    {
+        'author': 'author2',
+        'title': 'Post 2',
+        'content': 'Second content',
+        'date_posted': 'August 1, 2019'
+    }
+]
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"  
+    return render_template('about.html', title='About')
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     app.run(debug=True)
