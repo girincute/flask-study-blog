a03224da28834785ea97982eb43c899cce3eba84
Yebin Kim <girincute@gmail.com>
2019-08-02 23:18:45 +0900

    Add login page(validate)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, redirect
-from forms import RegistrationForm
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '4b8ab4beeca6bd7f6e6e443df836271d'
@@ -37,6 +37,19 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect('/home')
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect('/home')
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
