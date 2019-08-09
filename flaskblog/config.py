805fb7f90463ede821b19ed01fe6e041032b5425
Yebin Kim <girincute@gmail.com>
2019-08-10 06:30:28 +0900

    Use blueprint

---
+++ flaskblog/config.py
@@ -0,0 +1,8 @@
+class Config:
+    SECRET_KEY = '4b8ab4beeca6bd7f6e6e443df836271d'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
