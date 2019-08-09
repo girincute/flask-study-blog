5fca53c84c6dced73df9e7db901a083515c0be81
Yebin Kim <girincute@gmail.com>
2019-08-10 00:06:34 +0900

    Add user login authentication

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4b8ab4beeca6bd7f6e6e443df836271d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskblog import routes
