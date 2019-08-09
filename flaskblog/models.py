a5db9d7be85a1848375f376250d74c5ec8e21dad
Yebin Kim <girincute@gmail.com>
2019-08-10 06:40:12 +0900

    Add custom error pages

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -4,6 +4,7 @@ from flask import current_app
 from flaskblog import db, login_manager
 from flask_login import UserMixin
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
